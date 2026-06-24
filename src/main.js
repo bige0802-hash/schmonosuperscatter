387db4e776d2ed02b06bc80296c3af7d58f93412
bige0802-hash <bige0802@gmail.com>
2026-06-24 00:22:02 -0400

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { Game } from './game/game.js';
-
-window.addEventListener('load', async () => {
-  const game = new Game();
-  game.init();
-  await new Promise(r => setTimeout(r, 500));
-  document.getElementById('preloader')?.classList.add('done');
-});
